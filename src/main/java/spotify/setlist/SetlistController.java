8d39fca8cf04481c61e257836ca5ebbb41815369
Selbi <sebpet182@gmail.com>
2023-06-01 18:24:10 +0200

    Reverted default options thing (I did a stupid)

--- src/main/java/spotify/setlist/SetlistController.java
+++ src/main/java/spotify/setlist/SetlistController.java
@@ -19,8 +19,6 @@ import spotify.setlist.util.SetlistUtils;
 
 @RestController
 public class SetlistController {
-  private static final String DEFAULT_OPTIONS = "tapes,covers,medleys";
-
   private final SetlistCreator setlistCreator;
 
   SetlistController(SetlistCreator setlistCreator) {
@@ -34,9 +32,7 @@ public class SetlistController {
 
   @CrossOrigin
   @RequestMapping("/create")
-  public ResponseEntity<SetlistCreationResponse> createSpotifySetlistFromSetlistFmByParam(
-      @RequestParam("url") String url,
-      @RequestParam(value = "options", defaultValue = DEFAULT_OPTIONS) String options)
+  public ResponseEntity<SetlistCreationResponse> createSpotifySetlistFromSetlistFmByParam(@RequestParam("url") String url, @RequestParam(value = "options") String options)
     throws MalformedURLException, NotFoundException, IndexOutOfBoundsException {
     String setlistFmId = SetlistUtils.getIdFromSetlistFmUrl(url);
     SetlistCreationResponse setlistCreationResponse = setlistCreator.createSetlist(setlistFmId, options);
